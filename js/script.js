ea58f51f99689db8b95a8f9704797d14e49c2d78
Weverton Santos <wever-santos@hotmail.com>
2022-08-24 11:24:42 -0300

    Adicionando efeito máquina e cores

---
+++ js/script.js
@@ -0,0 +1,24 @@
+/* Animação nome */
+
+function textoMovendo(texto){
+
+    const textoArray = texto.innerHTML.split("");
+    texto.innerHTML = '';
+    
+    textoArray.forEach(function (letra, i){
+        setTimeout(function (){
+            texto.innerHTML += letra;
+        }, 100 * i);
+    });
+
+}
+
+const textoOla = document.querySelector('.hello');
+const textoSejaBem = document.querySelector('.bemvindo');
+const textoWeverton = document.querySelector('.name');
+textoMovendo(textoOla);
+textoMovendo(textoSejaBem);
+textoMovendo(textoWeverton);
+
+/*  */
+
